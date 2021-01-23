47b8bbb23fa2cb134659f400c42f30ab062a3532
ginowine <ginowine1@gmail.com>
2021-01-23 19:27:54 +0100

    removing duplicates from ArrayList

---
+++ src/RemoveDuplicatesFromArrayList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicatesFromArrayList {
+    public static void main(String[] args) {
+
+        // ArrayList with duplicate elements
+        ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(1, 1, 2, 3, 3, 3, 4, 5, 6, 6, 6, 7, 8));
+
+        System.out.println("ArrayList with duplicate elements: ", numbersList);
+
+        LinkedHashSet<Integer> hashSet = new LinkedHashSet<>(numbersList);
+        ArrayList<Integer> listWithoutDuplicates = new ArrayList<>(hashSet);
+
+        System.out.println("ArrayList without duplicate elements: ", listWithoutDuplicates);
+    }
+}
