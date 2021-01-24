8d7c294367d711f2b08e7b0eb027dfe7bea7a164
ginowine <ginowine1@gmail.com>
2021-01-24 06:04:48 +0100

    list iterator and iterator example

---
+++ src/IteratorExample.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class IteratorExample {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        // Iterator
+        Iterator itr = list.iterator();
+        System.out.println("Iterator:");
+        System.out.println("Forward traversal: ");
+
+        while (itr.hasNext())
+            System.out.print(itr.next() + " ");
+
+        // ListIterator
+        ListIterator i = list.listIterator();
+        System.out.println("\nListIterator:");
+        System.out.println("Forward Traversal : ");
+
+        while (i.hasNext())
+            System.out.print(i.next() + " ");
+
+        System.out.println("\nBackward Traversal : ");
+
+        while (i.hasPrevious())
+            System.out.print(i.previous() + " ");
+    }
+}
---
+++ src/UsingIteretors.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class UsingIteretors {
+    public static void main(String args[]){
+        ArrayList<Integer> it = new ArrayList<>();
+
+        it.add(7567);
+        it.add(8433);
+        it.add(7890);
+        it.add(2354);
+
+        Iterator<Integer> tt = it.iterator();
+
+        while (tt.hasNext()){
+            Integer integer = (Integer) tt.next();
+            it.add(2344); //This will throw ConcurrentModificationException
+        }
+    }
+}
