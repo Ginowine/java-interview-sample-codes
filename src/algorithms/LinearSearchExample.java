981ccf3f906ad4e85cb10ddf4b8478b71d6bbb8e
ginowine <ginowine1@gmail.com>
2021-03-12 09:18:50 +0100

    simple Linear search Algorithm implemented

---
+++ src/algorithms/LinearSearchExample.java
@@ -0,0 +1,20 @@
+package algorithms;
+
+public class LinearSearchExample {
+
+    public static int linearSearch(int[] arr, int key){
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        int[] numbers = {10, 23, 33, 30, 50, 100, 7, 5};
+        int key = 100;
+
+        System.out.println(key + "is found at index: " + linearSearch(numbers, key));
+    }
+}
