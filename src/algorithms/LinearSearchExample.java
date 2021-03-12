ac85b479824a8ad9df091652976d37e860327805
ginowine <ginowine1@gmail.com>
2021-03-12 09:45:09 +0100

    linear search with user input

--- src/algorithms/LinearSearchExample.java
+++ src/algorithms/LinearSearchExample.java
@@ -13,8 +13,8 @@ public class LinearSearchExample {
 
     public static void main(String[] args){
         int[] numbers = {10, 23, 33, 30, 50, 100, 7, 5};
-        int key = 100;
+        int key = 1;
 
-        System.out.println(key + "is found at index: " + linearSearch(numbers, key));
+        System.out.println(key + " " + "is found at index: " + linearSearch(numbers, key));
     }
 }
