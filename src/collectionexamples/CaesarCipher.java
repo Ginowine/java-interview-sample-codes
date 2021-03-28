5df8f9012fc2dc63a96e381aa4a3d946c6ba2d5d
ginowine <ginowine1@gmail.com>
2021-03-28 04:11:24 +0100

    decryption method implemented

--- src/collectionexamples/CaesarCipher.java
+++ src/collectionexamples/CaesarCipher.java
@@ -2,8 +2,8 @@ package collectionexamples;
 
 public class CaesarCipher {
 
-    public static StringBuffer encrypt(String text, int shift){
-        StringBuffer result = new StringBuffer();
+    public static String encrypt(String text, int shift){
+        StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < text.length(); i++){
             if (Character.isUpperCase(text.charAt(i))){
@@ -12,19 +12,29 @@ public class CaesarCipher {
             }else {
                 char ch = (char)(((int)text.charAt(i) +
                         shift - 97) % 26 + 97);
-                result.append(ch);
+                result.append(ch).toString();
             }
         }
 
-        return result;
+        return result.toString();
+    }
+
+    public static String decrypt(String text, int shift){
+        return encrypt(text, 26 - (shift % 26));
     }
 
     public static void main(String[] args)
     {
         String text = "ATTACKATONCE";
+        String text2 ="ro dyvn wo s myevn xofob dokmr k vvkwk dy nbsfo";
         int s = 4;
         System.out.println("Text  : " + text);
         System.out.println("Shift : " + s);
-        System.out.println("Cipher: " + encrypt(text, s));
+
+        String result = encrypt(text, s);
+        System.out.println("Cipher: " + result);
+        //System.out.println("Cipher: " + encrypt(text, s));
+
+        System.out.println("Decipher: " + decrypt(result, 4));
     }
 }
